7e1302cf76cbdd83bdf3879d0b505316bc266cd6
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2022-05-30 00:02:58 +0300

    c3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hw-2/",
   plugins: [vue()],
   resolve: {
     alias: {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/hw-2'
-    : '/'
-  }
+    publicPath: '/hw-2/'
+}
